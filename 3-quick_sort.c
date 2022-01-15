d22af556a14b20fff9904faf2d84542af4868e34
Dereje Desta <ddesta777@gmail.com>
2022-01-16 00:27:56 +0300

    added 1 and 3

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,68 +1,84 @@
 #include "sort.h"
 
 /**
- * swap - swaps a and b
- * @a: pointer a
- * @b: pointer b
+ * quick_sort - sorts @array by calling quick_sorter
+ * @array: array to be sorted
+ * @size: size of the array
+ *
+ * Return: void
  */
-void swap(int* a, int* b)
+void quick_sort(int *array, size_t size)
 {
-    int t = *a;
-    *a = *b;
-    *b = t;
-}
- 
-/**
- * partition - same in both iterative and recursive
- * @arr: list
- * @l: low index
- * @h: high index
- * Return: partition index
- */
-int partition(int arr[], int l, int h)
-{
-    int x = arr[h];
-    int i = (l - 1);
- 
-    for (int j = l; j <= h - 1; j++) {
-        if (arr[j] <= x) {
-            i++;
-            swap(&arr[i], &arr[j]);
-        }
-    }
-    swap(&arr[i + 1], &arr[h]);
-    return (i + 1);
+	quick_sorter(array, 0, (int)size - 1, (int)size);
 }
 
 /**
- * quick_sort - sorts an array in asc order
- * @list: array
- * @size: size of array
+ * quick_sorter - sorts @array using quick sort's Lomuto partition scheme and
+ * prints the array each time the pivot is swapped with the end item
+ * @array: array to be sorted
+ * @lo: lower boundary
+ * @hi: higher boundary
+ * @size: size of the array, passed to partition to print the array
+ *
+ * Return: void
  */
-void quick_sort(int *list, size_t size)
+void quick_sorter(int *array, int lo, int hi, int size)
 {
-    size_t l = 0, h = size - 1;
-    int stack[h - l + 1];
- 
-    int top = -1;
- 
-    stack[++top] = l;
-    stack[++top] = h;
- 
-    while (top >= 0) {
-        h = stack[top--];
-        l = stack[top--];
- 
-        int p = partition(list, l, h);
- 
-        if (p - 1 > l) {
-            stack[++top] = l;
-            stack[++top] = p - 1;
-        }
- 
-        if (p + 1 < h) {
-            stack[++top] = p + 1;
-            stack[++top] = h;
-        }
-    }
+	int pivot;
+
+	if (lo >= hi || lo < 0)
+		return;
+
+	pivot = partition(array, lo, hi, size);
+
+	quick_sorter(array, lo, pivot - 1, size);
+	quick_sorter(array, pivot + 1, hi, size);
+}
+
+/**
+ * partition - partitions @array between @lo and @hi and finds the center
+ * sport for @pivot by making sure the elements beneath and beyond @i are
+ * smaller and larger (or equal to) @pivot, between @lo and @hi
+ * @array: array to be sorted
+ * @lo: lower boundary
+ * @hi: higher boundary
+ * @size: size of the array, to print when @pivot has found its right place
+ *
+ * Return: the new position of the pivot
+ */
+int partition(int *array, int lo, int hi, int size)
+{
+	int pivot = array[hi]; /* Last element is chosen as the pivot */
+	int i = lo - 1;		   /* Temporary pivot index, used to find its final index */
+	int j;				   /* To scroll through the array */
+	int c;				   /* To swap array elements */
+
+	for (j = lo; j < hi; j++)
+	{
+		if (array[j] <= pivot)
+		{
+
+			i += 1; /* Move the temporary index forward */
+
+			/**
+			 * Swap the current element with the element at the temporary
+			 * pivot index
+			 */
+			c = array[i];
+			array[i] = array[j];
+			array[j] = c;
+		}
+	}
+
+	/**
+	 * Move pivot to the correct position, between the smaller and larger
+	 * elements
+	 */
+	i += 1;
+	c = array[hi];
+	array[hi] = array[i];
+	array[i] = c;
+	print_array(array, size);
+
+	return (i);
 }
--- sort.h
+++ sort.h
@@ -15,9 +15,17 @@ typedef struct listint_s {
 } listint_t;
 
 void print_array(const int *array, size_t size);
-void print_list(listint_t **head);
+void print_list(const listint_t *head);
 
 void bubble_sort(int *list, size_t size);
 void selection_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void quick_sort(int *list, size_t size);
+int are_they_neighbours(listint_t *A, listint_t *B);
+void refresh_outer_pointers(listint_t *A);
+void swap(listint_t *A, listint_t *B);
+void quick_sorter(int *array, int lo, int hi, int size);
+listint_t *get_head(listint_t *list);
+int partition(int *array, int lo, int hi, int size);
 
 #endif
